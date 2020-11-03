f3fc5ed4d6980cfe47e8eafb937a13787dc9956b
Daria <tityaevadash@gmail.com>
2020-11-03 19:00:14 +0300

    отредактировала кнопки

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ $(window).load(function () {
     $('.navbar-btn').on('click', function() {
          console.log($('.navbar-btn'));
         $('.header-wrap').toggleClass('header-wrap__active');
-        //$('.go-in').removeClass('.go-in__hidden');
-        //$('.go-in').removeClass('.go-in__hidden').toggleClass('go-in__active');
-       // $('.menu__nav').toggleClass('.menu__nav__hidden').toggleClass('menu__nav__active');
         $('.menu__nav').toggleClass('menu__nav__hidden menu__nav__active');
         $('.go-in').toggleClass('go-in__hidden go-in__active');
     });
